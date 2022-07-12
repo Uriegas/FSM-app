3ee9f70f8f9a766ed38b154a70133a8719145bdc
uriegas <1930526@upv.edu.mx>
2022-07-12 11:01:05 -0500

    Draw arrow head

--- app/src/main/java/com/upv/pm_2022/iti_27849_u2_equipo_04/Arrow.java
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u2_equipo_04/Arrow.java
@@ -3,12 +3,15 @@ package com.upv.pm_2022.iti_27849_u2_equipo_04;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.Log;
 
 public class Arrow extends Figure {
-    // TODO: Use a boolean variable to change the direction of the line
+    // TODO: Arched arrow
     public int endX;
     public int endY;
     private final Paint paint = new Paint();
+    private final Paint p_fill= new Paint();
 
     public Arrow(int id, int x, int y) {
         this.id=id; this.x=x; this.y=y; this.flag = false; // Flag used to change direction of arrow
@@ -16,29 +19,52 @@ public class Arrow extends Figure {
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(2.5f);
+        p_fill.setStyle(Paint.Style.FILL);
+        p_fill.setColor(Color.BLACK);
+        p_fill.setStrokeWidth(2.5f);
+        setFlag();
     }
 
     public void draw(Canvas canvas){
-        // Andrea, el codigo comentado es para hacer una linea curva, por el momento no hagas las
-        // lineas curvas, es mas facil con el que esta alli
-//        canvas.drawArc(this.x, this.y,this.x+200,this.y+200,(float)100,(float)10,false,paint);
-        canvas.drawLine(this.x, this.y, this.x+this.endX, this.y+this.endY, paint);
-        // TODO: draw triangle at end of arc (complete arrow)
-        // Andrea, esto de dibujar el triangulito en la punta de la flecha dejalo para el final
+        //canvas.drawArc(this.x, this.y,this.x+200,this.y+200,(float)100,(float)10,false,paint);
+        Path path = new Path();
+        path.moveTo(this.endX, this.endY);
+        path.lineTo(this.x, this.y);
+        canvas.drawPath(path, paint);
+        // Draw Head of the Arrow (triangle) - Get the angle
+        drawArrowHead(canvas, this.endX, this.endY, Math.atan2(this.endY-this.y, this.endX-this.x));
     }
 
-    public int onDown(int touchX, int touchY){
-        return this.id;
-    }
+    public int onDown(int touchX, int touchY){ onMove(touchX, touchY); return this.id; }
 
     public void onMove(int touchX, int touchY){ this.endX = touchX; this.endY = touchY; }
 
     /**
-     * Change direction of the arrow
-     * TODO: this function
+     * Changes the direction of the arrow by swapping the start and end coordinates
      */
     @Override
     public void setFlag() {
         this.flag = !this.flag;
+        // O(0) memory usage swap
+        this.x = this.x + this.endX; this.endX = this.x - this.endX; this.x = this.x - this.endX;
+        this.y = this.y + this.endY; this.endY = this.y - this.endY; this.y = this.y - this.endY;
+    }
+
+    /**
+     * Draws a triangle pointing to the direction of the drawn arrow
+     * @param canvas canvast draw in
+     * @param x coordinate of the last point of the arrow
+     * @param y coordinate of the last point of the arrow
+     * @param alpha angle from the cartesian plane to the given coordinate (x,y)
+     */
+    private void drawArrowHead(Canvas canvas, int x, int y, double alpha) {
+        double dx = Math.cos(alpha);
+        double dy = Math.sin(alpha);
+
+        Path path = new Path();
+        path.moveTo(x,y);
+        path.lineTo((float)(x - 24 * dx + 15 * dy), (float)(y - 24 * dy - 15 * dx));
+        path.lineTo((float)(x - 24 * dx - 15 * dy), (float)(y - 24 * dy + 15 * dx));
+        canvas.drawPath(path, p_fill);
     }
 }
